8ee8fe5af46a385353215c4f5812957bc6fe7441
emmanuel3wa <emmanuel.bebe@3wa.io>
2022-03-01 16:50:11 +0100

    [Feature] Add SplashScreen
    Add SplashScreenActivity and activity_splashscreen.xml
    This two files hundle the splashcreen.
    Change AndroidManifest.xml by adding SplashScreenActivity
    SplashScreenActivity is now the LAUNCHER activity

---
+++ app/src/main/java/com/example/hello_mariage/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.hello_mariage;
+
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        ImageView splashImage = findViewById(R.id.splash_image);
+
+        Animation animation = new AlphaAnimation(1, 0); //to change visibility from visible to invisible
+        animation.setDuration(1000); //1 second duration for each animation cycle
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setRepeatCount(Animation.INFINITE); //repeating indefinitely
+        animation.setRepeatMode(Animation.REVERSE);
+        splashImage.startAnimation(animation);
+    }
+}
