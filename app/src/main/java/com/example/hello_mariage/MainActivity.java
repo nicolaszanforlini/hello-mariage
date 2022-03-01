81b3933320e1dbe71af8945affff340974d8b7c5
nicolaszanforlini <nicolas.zanforlini1@gmail.com>
2022-03-01 17:04:16 +0100

    Menu principal buttons
    Add menu principal buttons and buttons listeners in mainActivity, mainActivity.xml.
    Add a border color drawable for menu principal buttons in directory drawable

--- app/src/main/java/com/example/hello_mariage/MainActivity.java
+++ app/src/main/java/com/example/hello_mariage/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hello_mariage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /* test init project, integration continue */
-    }
-}
 
+        // menu principal buttons
+        Button btnAccueil = findViewById(R.id.btnAccueil);
+        Button btnThemes = findViewById(R.id.btnThemes);
+        Button btnDevis = findViewById(R.id.btnDevis);
+        btnAccueil.setOnClickListener(btnAccueilListener);
+        btnThemes.setOnClickListener(btnThemesListener);
+        btnDevis.setOnClickListener(btnDevisListener);
+        // end principal buttons
+
+    }
+
+    // listener menu principal button
+    private final View.OnClickListener btnAccueilListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "bouton accueil", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private final View.OnClickListener btnThemesListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "bouton theme", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private final View.OnClickListener btnDevisListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "bouton devis", Toast.LENGTH_SHORT).show();
+        }
+    };
+    // end listener menu principal button
+
+}
